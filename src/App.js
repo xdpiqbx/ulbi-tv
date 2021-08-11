b1a25a9052656ee61f37dd909a4ea68fb5a1f9d5
xdpiqbx <xdpiqbx@gmail.com>
2021-08-11 06:44:49 +0300

    added setLikes state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from 'react';
 
 export default function App() {
+  const [likes, setLikes] = useState(0);
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+  function decrement() {
+    setLikes(likes - 1);
+  }
   return (
     <div className="App">
-      <h1>It Works!</h1>
+      <h1>{likes}</h1>
+      <button onClick={increment}>Incr</button>
+      <button onClick={decrement}>Decr</button>
     </div>
   );
 }
