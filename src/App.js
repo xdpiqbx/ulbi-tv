92aa9209f4a1a19ecc00c76122b8e7c690c16368
xdpiqbx <xdpiqbx@gmail.com>
2021-08-11 06:57:36 +0300

    added input state

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 export default function App() {
   const [likes, setLikes] = useState(0);
+  const [value, setValue] = useState('Input placeholder');
 
   function increment() {
     setLikes(likes + 1);
@@ -14,6 +15,15 @@ export default function App() {
       <h1>{likes}</h1>
       <button onClick={increment}>Incr</button>
       <button onClick={decrement}>Decr</button>
+      <hr />
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => {
+          setValue(event.target.value);
+        }}
+      />
     </div>
   );
 }
