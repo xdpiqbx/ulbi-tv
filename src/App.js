26dab1364f60b9ddf0202a9162c3edcfbfdcb6c6
xdpiqbx <xdpiqbx@gmail.com>
2021-08-11 08:31:01 +0300

    added Post List component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import PostItem from './components/PostItem';
+import React, { useState } from 'react';
+import PostList from './components/PostList';
+
 import './styles/App.css';
 
 export default function App() {
+  const [posts1, setPosts1] = useState([
+    {
+      id: 1,
+      title: 'Javascript',
+      body: 'Javascript - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, officia.',
+    },
+    {
+      id: 2,
+      title: 'React',
+      body: 'React - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, officia.',
+    },
+  ]);
+  const [posts2, setPosts2] = useState([
+    {
+      id: 1,
+      title: 'Python',
+      body: 'Python - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, officia.',
+    },
+    {
+      id: 2,
+      title: 'Django',
+      body: 'Django - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, officia.',
+    },
+  ]);
   return (
     <div className="App">
-      <PostItem
-        post={{
-          id: 1,
-          title: 'Javascript',
-          body: 'Javascript - Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore, officia.',
-        }}
-      />
+      <PostList posts={posts1} title="Список постов JS" />
+      <PostList posts={posts2} title="Список постов Python" />
     </div>
   );
 }
---
+++ src/components/PostList.jsx
@@ -0,0 +1,13 @@
+import PostItem from './PostItem';
+
+const PostList = ({posts, title}) => {
+  return(
+    <div>
+      <h1 style={{ textAlign: 'center' }}>{title}</h1>
+      {posts.map((post) => (
+        <PostItem post={post} key={post.id} />
+      ))}
+    </div>
+  )
+}
+export default PostList
