04a74fec669109048d8bcf7a40ef0aca9841ca58
xdpiqbx <xdpiqbx@gmail.com>
2021-08-14 22:44:57 +0300

    added custom hook useFetching

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -2,12 +2,8 @@ import axios from 'axios';
 
 export default class PostService {
   static async getAll() {
-    try {
-      const URI = 'https://jsonplaceholder.typicode.com/posts';
-      const response = await axios.get(URI, {});
-      return response.data;
-    } catch (error) {
-      console.log(error);
-    }
+    const URI = 'https://jsonplaceholder.typicode.com/posts';
+    const response = await axios.get(URI, {});
+    return response.data;
   }
 }
--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
 import PostService from './API/PostService';
 import Loader from './components/UI/Loader/Loader';
+import { useFetching } from './hooks/useFetching';
 
 export default function App() {
   const [posts, setPosts] = useState([]);
@@ -23,19 +24,16 @@ export default function App() {
     filter.searchQuery
   );
 
-  const [isPostLoading, setIsPostLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(
+    async () => {
+      setPosts(await PostService.getAll());
+    }
+  );
 
   useEffect(() => {
     fetchPosts();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
-  const fetchPosts = async () => {
-    setIsPostLoading(true);
-    setTimeout(async () => {
-      setPosts(await PostService.getAll());
-      setIsPostLoading(false);
-    }, 1000);
-  };
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
@@ -62,7 +60,13 @@ export default function App() {
       </MyModal>
       <hr style={{ margin: '15px 0' }} />
       <PostFilter filter={filter} setFilter={setFilter} />
-      {isPostLoading ? (
+      {postError && (
+        <>
+          <h1>Произошла ошибка</h1>
+          <p>{postError}</p>
+        </>
+      )}
+      {isPostsLoading ? (
         // <h1 style={{ textAlign: 'center' }}>Loading...</h1>
         <div
           style={{
