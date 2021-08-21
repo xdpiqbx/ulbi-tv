78e85038dbe00a2e3b72936c5b968f9bc607a20d
xdpiqbx <xdpiqbx@gmail.com>
2021-08-21 22:17:53 +0300

    work with array customers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 function App() {
   const dispatch = useDispatch();
   const stateCash = useSelector((state) => state.rCash.cash);
+  const stateCustomers = useSelector((state) => state.rCustomer.customers);
 
   const addCash = (cash) => {
     dispatch({ type: 'ADD_CASH', payload: cash });
@@ -11,12 +12,52 @@ function App() {
   const getCash = (cash) => {
     dispatch({ type: 'GET_CASH', payload: cash });
   };
+  const addCustomer = (name) => {
+    const customer = {
+      id: Date.now(),
+      name,
+    };
+    dispatch({ type: 'ADD_CUSTOMER', payload: customer });
+  };
+
+  const removeCustomer = (customer) => {
+    dispatch({ type: 'REMOVE_CUSTOMER', payload: customer.id });
+  };
 
   return (
     <div>
       <div style={{ fontSize: '3rem' }}>{stateCash}</div>
       <button onClick={() => addCash(parseInt(prompt('Add')))}>Incr</button>
       <button onClick={() => getCash(parseInt(prompt('Get')))}>Decr</button>
+      <button onClick={() => addCustomer(prompt('Добавить клинета'))}>
+        Добавить клинета
+      </button>
+      <button onClick={() => getCash(parseInt(prompt('Удалить клинета')))}>
+        Удалить клинета
+      </button>
+      {stateCustomers.length > 0 ? (
+        <ul style={{ listStyle: 'none' }}>
+          {stateCustomers.map((customer) => (
+            <li
+              onClick={() => removeCustomer(customer)}
+              style={{
+                fontSize: '2rem',
+                border: '1px solid black',
+                margin: '5px',
+                padding: '5px',
+                cursor: 'pointer',
+              }}
+              key={customer.name}
+            >
+              {customer.name}
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div style={{ fontSize: '3rem', marginTop: 20 }}>
+          Клиентов нет {`:(`}
+        </div>
+      )}
     </div>
   );
 }
--- src/store/customerReducer.js
+++ src/store/customerReducer.js
@@ -4,8 +4,13 @@ export const customerReducer = (state = defaultState, action) => {
   switch (action.type) {
     case 'ADD_CUSTOMER':
       return { ...state, customers: [...state.customers, action.payload] };
-    case 'GET_CUSTOMER':
-      return state.customers[action.payload];
+    case 'REMOVE_CUSTOMER':
+      return {
+        ...state,
+        customers: state.customers.filter(
+          (customer) => customer.id !== action.payload
+        ),
+      };
     default:
       return state;
   }
