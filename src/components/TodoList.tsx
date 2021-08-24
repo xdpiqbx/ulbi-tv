63846fa3c0a2cd09ed729d040bf7a35f22ed5695
xdpiqbx <xdpiqbx@gmail.com>
2021-08-24 09:54:06 +0300

    added simple pagination

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -7,14 +7,14 @@ const TodoList: React.FC = () => {
     (state) => state.rTodo
   );
 
-  const { fetchTodos } = useActions();
+  const { fetchTodos, setTodoPage } = useActions();
 
   const pages = [1, 2, 3, 4, 5];
 
   useEffect(() => {
     fetchTodos(page, limit);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [page]);
 
   if (loading) {
     return <h1>Loading...</h1>;
@@ -25,11 +25,28 @@ const TodoList: React.FC = () => {
 
   return (
     <div>
-      {todos.map((todo) => (
-        <div key={todo.id}>
-          {todo.id} - {todo.title}
-        </div>
-      ))}
+      <div>
+        {todos.map((todo) => (
+          <div key={todo.id}>
+            {todo.id} - {todo.title}
+          </div>
+        ))}
+      </div>
+      <ul style={{ display: 'flex', margin: 0, padding: 0 }}>
+        {pages.map((p) => (
+          <li
+            onClick={() => setTodoPage(p)}
+            style={{
+              listStyle: 'none',
+              padding: '5px 10px',
+              border: p === page ? '2px solid green' : '1px solid gray',
+            }}
+            key={p}
+          >
+            {p}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
